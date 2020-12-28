fa5936184740dc8758b0ea5cfbfb06fd4102e3bc
Hidemitsu Aoki <olympos0117@gmail.com>
2020-12-28 14:28:07 +0900

    Add todos to post  as api

--- pages/api/todos.ts
+++ pages/api/todos.ts
@@ -2,6 +2,12 @@ import { NextApiRequest, NextApiResponse } from "next";
 import { PrismaClient } from "@prisma/client";
 import { getSession } from "next-auth/client";
 const prisma = new PrismaClient();
+
+type Data = {
+  title: string;
+  body: string;
+};
+
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   const session = await getSession({ req });
   if (!session) return res.status(401).end("Please log in to view");
@@ -17,4 +23,21 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
     });
     return res.status(200).json(todos);
   }
+
+  if (req.method === "POST") {
+    const { title, body } = JSON.parse(req.body) as Data;
+    const createdTodo = await prisma.todo.create({
+      data: {
+        title,
+        body,
+        User: {
+          connect: {
+            id: userId,
+          },
+        },
+      },
+    });
+    res.status(201).json(createdTodo);
+  }
+
 };
