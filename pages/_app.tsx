dc3b91b42fd3f04f12c788b725cf0e981e9c387a
Hidemitsu Aoki <olympos0117@gmail.com>
2020-12-28 14:36:18 +0900

    Wrap component by QueryClientProvider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,18 @@
 import '../styles/globals.css'
 import { Provider } from "next-auth/client";
+import { QueryClient, QueryClientProvider } from "react-query";
+
+const queryClient = new QueryClient();
 
 function MyApp({ Component, pageProps }) {
+  console.log("queryClient", queryClient)
+  console.log("pageProps", pageProps)
   return (
-    <Provider session={pageProps.session}>
-      <Component {...pageProps} />
-    </Provider>
+    <QueryClientProvider client={queryClient}>
+      <Provider session={pageProps.session}>
+        <Component {...pageProps} />
+      </Provider>
+    </QueryClientProvider>
   )
 }
 
