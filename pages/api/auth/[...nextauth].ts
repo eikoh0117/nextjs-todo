58f6ccaa89729b25f1eb9d64523bf0a084a10181
Hidemitsu Aoki <olympos0117@gmail.com>
2020-12-28 13:09:14 +0900

    Declare prisma

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -3,18 +3,7 @@ import Providers from "next-auth/providers";
 import Adapters from "next-auth/adapters";
 import { PrismaClient } from "@prisma/client";
 let prisma;
-// ローカルでは大量にデータベースコネクションを張ってしまうことがあるので、
-// このようなアプローチをとる。TypeScript が global type に prisma がないと怒るので、
-// ルートディレクトリに global.d.ts を作成し、
-// export {};
-// declare global {
-//   namespace NodeJS {
-//     interface Global {
-//       prisma: any;
-//     }
-//   }
-// }
-// としてあげれば治る
+
 if (process.env.NODE_ENV === "production") {
   prisma = new PrismaClient();
 } else {
@@ -29,13 +18,6 @@ const options = {
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
-    //　プロバイダーは何個でも指定できる。
-    // https://next-auth.js.org/getting-started/introduction で一覧がみれる
-    // 例：
-    // Providers.Twitter({
-    //   clientId: process.env.TWITTER_CLIENT_ID,
-    //   clientSecret: process.env.TWITTER_CLIENT_SECRET,
-    // }),
   ],
   adapter: Adapters.Prisma.Adapter({ prisma }),
 };
